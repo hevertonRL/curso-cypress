23f52ae170be7d86f332f3ab5af048ee24101939
hevertonRL <heverton_rl@hotmail.com>
2023-10-17 08:28:36 -0300

    Final da aula 12

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -1,7 +1,15 @@
 /// <reference types="cypress" />
 describe('Work with basic elements'),() => {
-    it('Text', () => {
+
+    before(() => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(()=>{
+        cy.reload()
+    })
+
+    it('Text', () => {
         cy.get('body').should('contain', 'Cuidado')
         //cy.get('body').should('have.text', 'Cuidado')
         cy.get('span').should('contain', 'Cuidado')
@@ -11,11 +19,9 @@ describe('Work with basic elements'),() => {
     })
 
     it('Links', () => {
-        cy.visit('https://wcaquino.me/cypress/componentes.html')
         cy.get('a').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
 
-        cy.reload()
         cy.get('#resultado').should('have.not.text', 'Voltou!')
         cy.contains('Voltar').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
